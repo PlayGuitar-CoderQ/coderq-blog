a107e91a22186029a512ae35c90ec2521cec4bed
coder_Q <1282117217@qq.com>
2022-02-17 18:04:04 +0800

    feat(global): ✨ 新增eslint规则, prettier代码美化, pre-commit钩子进行提交lint代码

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-        初始化版本
-    </div>
-  )
-}
+  return <div>初始化版本</div>;
+};
 
-export default Home
+export default Home;
